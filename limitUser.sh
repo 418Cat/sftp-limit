3fa373b921bf5e64402da640e830dcb9e8360d65
418Cat <94795008+418Cat@users.noreply.github.com>
2022-01-20 10:51:16 +0100

    Update limitUser.sh
    Passwords are no longer stored in the file itself and it's no longer needed to edit the file to change password/user limit

--- limitUser.sh
+++ limitUser.sh
@@ -1,22 +1,31 @@
-echo -e "PASSWORD\nPASSWORD" | passwd USER #Reset the password in case something goes wrong while the password is changed
+echo Please enter the user\'s name you want to limit
+read USER
+echo Please enter the user\'s password
+read PASSWORD
+clear
+echo Please enter a random long string \(just press all over your keyboard\)
+read RANDOMPASSWORD
+clear
+echo -e "$PASSWORD\n$PASSWORD" | passwd "$USER"
+echo How many users can connect at the same time \?
+read LIMIT
 B=0
-LIMIT=1 #Limit to one connection at the same time
 while true; do
 
         sleep 1
-        A=$(ps -aux | grep -v 'grep' | grep -c 'USER@notty')
+        A=$(ps -aux | grep -v 'grep' | grep -c "$USER@notty")
 
         if [ $((A)) -ge $LIMIT ]; then
 
-                if [ $B = 0 ]; then
-                        echo -e "RANDOM_PASSWORD\nRANDOM_PASSWORD" | passwd USER
-                        echo "The limit of USER connections was exceeded"
+                if [ $B == 0 ]; then
+			echo -e "$RANDOMPASSWORD\n$RANDOMPASSWORD" | passwd "$USER"
+                        echo "La limite de $LIMIT a été atteinte"
                         B=1
                 fi
         fi
         if [ $((A)) -lt $LIMIT ]; then
                 if [ $B == 1 ]; then
-                        echo -e "PASSWORD\nPASSWORD" | passwd USER
+                        echo -e "$PASSWORD\n$PASSWORD" | passwd "$USER"
                         B=0
                 fi
         fi
